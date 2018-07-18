a9a5089376b65319707f1cc06b08744175ffb7ea
Daniel Abernathy <dabernathy89@gmail.com>
2018-07-18 14:35:42 -0500

    database setup

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,6 +17,8 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'age' => $faker->numberBetween(18, 100),
+        'title' => $faker->randomElement(['Regional Manager', 'Assistant to the Regional Manager', 'Sales Associate']),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->integer('age');
+            $table->string('title');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
