85524b6282ccbc61846e9cac05556a2f03d1e451
Daniel Abernathy <dabernathy89@gmail.com>
2018-07-18 14:38:40 -0500

    parse VQB data

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,46 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+function parseQBGroup($query, $group, $method = 'where')
+{
+    $query->{$method}(function ($subquery) use ($group) {
+        $sub_method = $group['query']['logicalOperator'] === 'All' ? 'where' : 'orWhere';
+        foreach ($group['query']['children'] as $child) {
+            if ($child['type'] === 'query-builder-group') {
+                parseQBGroup($subquery, $child, $sub_method);
+            } else {
+                parseQBRule($subquery, $child, $sub_method);
+            }
+        }
+    });
+}
+
+function parseQBRule($query, $rule, $method)
+{
+    if ($rule['query']['rule'] === 'age') {
+        $query->{$method}('age', $rule['query']['selectedOperator'], $rule['query']['value']);
+    }
+
+    if ($rule['query']['rule'] === 'job_title') {
+        $query->{$method}('title', $rule['query']['selectedOperator'], $rule['query']['value']);
+    }
+}
+
+Route::post('/results', function (\Illuminate\Http\Request $request) {
+    $qb = $request->input('query');
+    $group = [];
+    $group['query'] = ['children' => $qb['children']];
+    $group['query']['logicalOperator'] = $qb['logicalOperator'];
+    $method = $qb['logicalOperator'] === 'All' ? 'where' : 'orWhere';
+
+    $users = \App\User::query();
+
+    parseQBGroup($users, $group, $method);
+
+    return $users->get()->all();
 });
 
 Auth::routes();
